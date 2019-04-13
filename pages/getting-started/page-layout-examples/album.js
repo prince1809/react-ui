af3e89deef0206cadc2c5ac512a7e55c0b76d4a3
prince <prince18pat@gmail.com>
2019-04-13 13:37:27 +0900

    add album layout

---
+++ pages/getting-started/page-layout-examples/album.js
@@ -0,0 +1,19 @@
+import 'docs/src/modules/components/bootstrap';
+// --- Post bootstrap -----
+import React from 'react';
+import AppTheme from 'docs/src/modules/components/AppTheme';
+import Album from 'docs/src/pages/getting-started/page-layout-examples/album/Album';
+
+function Page() {
+  return (
+    <AppTheme
+      title="Album Page layout - Material UI"
+      description="An example layout for creating an album or gallery"
+    >
+    <Album />
+    </AppTheme>
+  );
+}
+
+
+export default Page;
